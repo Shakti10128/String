bd60a4d84738749af6da302a6789a722a834b82a
shakti <kumarshakti2017@gmail.com>
2023-02-27 11:22:18 +0530

    first commit

---
+++ Isomorphic_Strings.cc
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isIsomorphic(string s, string t) {
+        int i = 1;
+        int l1 = s.length(),l2 = t.length();
+        map<char,char> m1;
+        map<char,char> m2;
+
+        if(s[0]!=t[0]){
+        m1[s[0]] = t[0];
+        m2[t[0]] = s[0];
+        }
+        while(i < l1){
+            auto s1 = m1.find(s[i]);
+            auto s2 = m2.find(t[i]);
+            if(s1==m1.end()){
+                m1[s[i]] = t[i];
+                m2[t[i]] = s[i];
+            }
+            if(s2!=m2.end() && (*s2).second!=s[i]){
+                return false;
+            }
+            else if(s1!=m1.end() && (*s1).second!=t[i]){
+                return false;
+            }
+            i++;
+        }
+        return true;
+}
+int main(){
+    string s = "foo";
+    string t = "bar";
+   bool ans =  isIsomorphic(s,t);
+   cout<<ans;
+
+   return 0;
+}
